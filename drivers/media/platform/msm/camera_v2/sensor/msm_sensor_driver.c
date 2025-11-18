ubports-clover/android_kernel_xiaomi_clover

drivers/media/platform/msm/camera_v2/sensor: Import Xiaomi changes

drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -17,6 +17,9 @@
 #include "camera.h"
 #include "msm_cci.h"
 #include "msm_camera_dt_util.h"
+#ifdef CONFIG_XIAOMI_CLOVER
+#include <linux/hardware_info.h>
+#endif
 
 /* Logging macro */
 #undef CDBG
@@ -1098,6 +1101,14 @@ CSID_TG:
 	 * Set probe succeeded flag to 1 so that no other camera shall
 	 * probed on this slot
 	 */
+#ifdef CONFIG_XIAOMI_CLOVER
+	if (0 == slave_info->camera_id)
+		get_hardware_info_data(HWID_MAIN_CAM,(void *)slave_info->sensor_name);
+	else {
+		if (2 == slave_info->camera_id)
+			get_hardware_info_data(HWID_SUB_CAM,(void *)slave_info->sensor_name);
+	}
+#endif
 	s_ctrl->is_probe_succeed = 1;
 	return rc;
 

Aeoniixx-Lavender/android_kernel_xiaomi_sdm660_southwest-ng

(cherry picked from commit c86c102f49456db82eae5a2a63e0043507cbf66a)
Signed-off-by: pix106 <sbordenave@gmail.com>

drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -24,6 +24,10 @@
 extern struct vendor_eeprom s_vendor_eeprom[CAMERA_VENDOR_EEPROM_COUNT_MAX];
 #endif
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+#include <linux/hardware_info.h>
+#endif
+
 /* Logging macro */
 #undef CDBG
 #define CDBG(fmt, args...) pr_debug(fmt, ##args)
@@ -1897,6 +1901,14 @@ CSID_TG:
 	 * Set probe succeeded flag to 1 so that no other camera shall
 	 * probed on this slot
 	 */
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	if (0 == slave_info->camera_id)
+		get_hardware_info_data(HWID_MAIN_CAM,(void *)slave_info->sensor_name);
+	else {
+		if (2 == slave_info->camera_id)
+			get_hardware_info_data(HWID_SUB_CAM,(void *)slave_info->sensor_name);
+	}
+#endif
 	s_ctrl->is_probe_succeed = 1;
 	return rc;
 